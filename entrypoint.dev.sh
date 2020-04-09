b2ec74f12b37400f3eed23d2681ff76b21b042eb
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-04-09 12:56:13 +0100

    Adding automatic admin user creation.

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -13,5 +13,6 @@ fi
 
 python manage.py flush --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
 
 exec "$@"
