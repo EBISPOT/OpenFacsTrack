d439caa94f32f100f616de54d72995a8b5b0db9a
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-04-18 10:43:13 +0100

    Adding authentication service.

--- openfacstrack/apps/track/urls.py
+++ openfacstrack/apps/track/urls.py
@@ -1,7 +1,13 @@
 """
 track app URL Configuration
 """
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("upload/", views.upload, name="upload"),
+    path("login/", views.index, name="index"),
+    url(r"^oidc/", include("mozilla_django_oidc.urls")),
+]
--- openfacstrack/apps/track/views.py
+++ openfacstrack/apps/track/views.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
 
 
 def index(request):
-    return HttpResponse("tracking app")
+    return render(request, "track/login.html")
+
+
+@login_required(login_url="/track/login/")
+def upload(request):
+    return render(request, "track/upload.html")
