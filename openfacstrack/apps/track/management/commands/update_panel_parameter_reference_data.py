9beaa9dcfff2f8fc546e2051580988699ff115c9
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-04-23 16:47:59 +0100

    Add linting tules to update_panel_parameter_reference_data

--- openfacstrack/apps/track/management/commands/update_panel_parameter_reference_data.py
+++ openfacstrack/apps/track/management/commands/update_panel_parameter_reference_data.py
@@ -4,27 +4,25 @@ from django.db import transaction
 
 import pandas as pd
 
-from openfacstrack.apps.track.models import (
-    Parameter,
-    Panel,
-)
+from openfacstrack.apps.track.models import Parameter, Panel
+
 
 class Command(BaseCommand):
-    help = 'Update the Panel and Parameter reference tables from file'
+    help = "Update the Panel and Parameter reference tables from file"
 
     def add_arguments(self, parser):
-        parser.add_argument('filename')
-        #parser.add_argument('skiprows')
+        parser.add_argument("filename")
+        # parser.add_argument('skiprows')
 
     def handle(self, *args, **options):
-        filename = options['filename']
-        #skiprows = options['skiprows']
+        filename = options["filename"]
+        # skiprows = options['skiprows']
         try:
             df_panels = pd.read_excel(filename, skiprows=1)
-        except Exception  as e:
+        except Exception as e:
             print(f"There was a problem loading {filename}. Error was: ")
             raise CommandError(str(e))
-        
+
         with transaction.atomic():
             # Store panel names in Panel table
             panel_names = [p.upper() for p in df_panels.panel.unique().tolist()]
@@ -37,8 +35,8 @@ class Command(BaseCommand):
             for index, row in df_panels.iterrows():
                 panel = Panel.objects.get(name=row["panel"].upper())
                 parameter = Parameter.objects.get_or_create(
-                    gating_hierarchy=row['gating hierarchy'],
-                    panel=panel)[0]
+                    gating_hierarchy=row["gating hierarchy"], panel=panel
+                )[0]
 
                 parameter.internal_name = row["marker string"]
                 parameter.public_name = row["public_population_name"]
@@ -50,4 +48,3 @@ class Command(BaseCommand):
                 # What is 'ancestral population when preseneted as fraction' ?
                 parameter.save()
                 print(parameter)
-
