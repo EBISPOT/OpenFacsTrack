83c14322c5415b847201086d5ecd8fdff782170c
Federico Lopez Gomez <federico@ebi.ac.uk>
2020-04-23 09:08:24 +0100

    Change dev Dockerfile from using alpine to use slim. Add pnale updates to the entrypoint.

--- docker/entrypoint.dev.sh
+++ docker/entrypoint.dev.sh
@@ -24,6 +24,7 @@ fi
 
 python manage.py flush --no-input
 python manage.py migrate
+python manage.py update_panel_parameter_reference_data sample_data/population_names_20200413.xlsx
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
 python manage.py collectstatic --no-input --clear
 
